1e9f57017d349c2924cc80728558a40bdc3b5083
Mariana Paulino <mariana23paulino@gmail.com>
2023-12-12 18:52:51 +0000

    Task 4 and 5 COMPLETE

---
+++ Labs#7/Task#4-5/lab7-5.py
@@ -0,0 +1,25 @@
+#Task #5 - Obtain the sorted amount spent by customer
+#Spark RDDs
+
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("TotalAmountSpent")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    customerID = int(fields[0])
+    amountSpent = float(fields[2])
+    return (customerID, amountSpent)
+
+lines = sc.textFile("./customer-orders.csv")
+rdd = lines.map(parseLine)
+
+totalAmountSpent = rdd.reduceByKey(lambda x, y: x + y)
+totalAmountSpentSorted = totalAmountSpent.map(lambda x: (x[1], x[0])).sortByKey()
+
+results = totalAmountSpentSorted.collect()
+
+for result in results:
+    print(result)
