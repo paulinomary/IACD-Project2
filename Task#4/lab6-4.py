038fe0140445d38ff14e9c76a55b34dcb8e56e86
Mariana Paulino <mariana23paulino@gmail.com>
2023-12-12 18:31:16 +0000

    Task 4 COMPLETE

---
+++ Task#4/lab6-4.py
@@ -0,0 +1,28 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class TotalAmountbyCustomer(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_amount,
+                   reducer=self.reducer_sum_amount),
+            MRStep(mapper=self.mapper_make_amount_key,
+                   reducer=self.reducer_output_amount)
+        ]
+
+    def mapper_get_amount(self, _, line):
+        (customerID, itemID, amountSpent) = line.split(',')
+        yield customerID, float(amountSpent)
+
+    def reducer_sum_amount(self, customerID, amounts):
+        yield customerID, sum(amounts)
+
+    def mapper_make_amount_key(self, customerID, totalAmount):
+        yield 'key', (totalAmount, customerID)
+
+    def reducer_output_amount(self, _, customer_amounts):
+        for totalAmount, customerID in sorted(customer_amounts, reverse=True):
+            yield customerID, totalAmount
+
+if __name__ == '__main__':
+    TotalAmountbyCustomer.run()
